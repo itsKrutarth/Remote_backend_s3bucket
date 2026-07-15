5884a1cbf45de736a876d2255585376c15a84f07
Krutarth Patel <mail2krutarthpatel@gmail.com>
2026-07-15 10:35:56 -0400

    few more provisions in bucket

--- s3Bucket.tf
+++ s3Bucket.tf
@@ -23,3 +23,20 @@ resource "aws_s3_bucket_versioning" "s3BucketVersioning" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_encryption" {
+  bucket = aws_s3_bucket.s3bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "tf_state_block_pub_access" {
+  bucket = aws_s3_bucket.s3bucket.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
